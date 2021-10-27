158e8a01ad73cc5034aeacacb3f225af3308e339
jonathan <jonathanwears@outlook.com>
2021-10-27 18:04:40 +0200

    added state to game component

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import { Button } from "@chakra-ui/react";
+import React, { useState, useEffect } from 'react';
+import { Button } from '@chakra-ui/react';
+import { getAllWords } from '../utils/index';
 
 function Game() {
+  const [flashcards, setFlashcards] = useState();
+  const [gameStart, setGame] = useState(false);
+  function startGame() {
+    setGame(!gameStart);
+  }
+
+  useEffect(() => {
+    async function getFlashcards() {
+      const req = await getAllWords();
+      setFlashcards(req);
+    }
+    getFlashcards();
+  }, []);
+
   return (
     <>
-      <Button type="button">Start</Button>
+      {!gameStart && <Button onClick={startGame}>Start</Button>}
+      {gameStart && flashcards.map((flashcard, index) => <p key={index}>{flashcard.englishWord}</p>)}
     </>
-
   );
 }
 
