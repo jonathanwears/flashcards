a3dd3d055310f2d2eee3ffc325172081a6e5b716
jonathan <jonathanwears@outlook.com>
2021-10-28 15:55:04 +0200

    added basic game functionality

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Button } from '@chakra-ui/react';
 import { getAllWords } from '../utils/index';
+import Flashcard from './FlashCard/FlashCard';
 
 function Game() {
   const [flashcards, setFlashcards] = useState();
   const [gameStart, setGame] = useState(false);
+  const [randomNumber, setRandomNumber] = useState(0);
   function startGame() {
     setGame(!gameStart);
   }
@@ -17,10 +19,23 @@ function Game() {
     getFlashcards();
   }, []);
 
+  function randomise() {
+    const number = Math.floor(Math.random() * flashcards.length);
+    setRandomNumber(number);
+  }
+
   return (
     <>
-      {!gameStart && <Button onClick={startGame}>Start</Button>}
-      {gameStart && flashcards.map((flashcard, index) => <p key={index}>{flashcard.englishWord}</p>)}
+      {!gameStart && flashcards && <Button onClick={startGame}>Start</Button>}
+      {
+        gameStart
+        && (
+        <>
+          <Flashcard word={flashcards[randomNumber]} />
+          <Button onClick={randomise}>Next Word</Button>
+        </>
+        )
+      }
     </>
   );
 }
