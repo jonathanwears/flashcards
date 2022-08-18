912fb47132d3417ad7ca1993ea8364907a19068e
Jona <jonathanwears@outlook.com>
2022-08-18 17:15:05 +0200

    fixed errors and optimisation

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,43 @@
 const path = require('path');
-const HtmlWebpackPlugin =require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
   // Where files should be sent once they are bundled
- output: {
-   path: path.join(__dirname, './dist'),
-   filename: 'index.bundle.js',
- },
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'index.bundle.js',
+  },
   // webpack 5 comes with devServer which loads in development mode
- devServer: {
-   port: 3000,
-   watchContentBase: true,
-   historyApiFallback: true
- },
- // Webpack doesnt check for jsx automatically.
-   resolve: {
-      extensions: ['.jsx', '.js', '.json']
+  devServer: {
+    port: 3000,
+    watchContentBase: true,
+    historyApiFallback: true
+  },
+  // Webpack doesnt check for jsx automatically.
+  resolve: {
+    extensions: ['.jsx', '.js', '.json']
   },
   // Rules of how webpack will take our files, complie & bundle them for the browser 
- module: {
-   rules: [
-     {
-       test: /\.(js|jsx)$/,
-       exclude: /node_Modules/,
-       use: {
-         loader: 'babel-loader',
-       }
-     },
-     {
-       test: /\.css$/,
-       use: ['style-loader', 'css-loader']
-     }
-   ]
- },
- plugins: [
-   new HtmlWebpackPlugin({
-    template: './src/index.html', 
-    inject: false
-  }),
-   new webpack.ProvidePlugin({
-    process: 'process/browser',
-  })
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_Modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: false
+    }),
   ],
 }
