3e0600389ef31fd943315b6fc5c8081e27c68737
Jona <jonathanwears@outlook.com>
2023-03-03 12:42:46 +0100

    implemented usedatastore

--- src/pages/Database.jsx
+++ src/pages/Database.jsx
@@ -1,27 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Grid } from '@chakra-ui/react';
 import FlashCard from '../components/FlashCard/FlashCard';
 import { getAllWords } from '../utils/server';
-import AddNewWordButton from '../components/AddNewWordButton';
-import mockData from '../../mockData';
+import useDataStore from '../utils/useDataStore';
+import AddNewWord from '../components/FlashCard/AddNewWord';
 
 function Database() {
+  const dataStore = useDataStore((state) => state.words);
   const [words, setWords] = useState();
 
   useEffect(() => {
     async function getData() {
       // const data = await getAllWords();
       // const data = mockData;
-      setWords(mockData);
+      setWords(dataStore);
     }
     getData();
-  }, []);
+  }, [dataStore]);
 
-  const renderWords = words ? words.map((word, index) => <FlashCard key={`${word}${index}`} word={word} />) : null
+  const renderWords = words ? words.map((word, index) => <FlashCard key={`${word}${index}`} index={index} word={word} />) : null
 
   return (
     <>
-      <AddNewWordButton />
+      <AddNewWord />
       <div className="flex">
         {renderWords}
       </div>
