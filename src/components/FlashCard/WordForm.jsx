829c88d382648d1f30d1fd7245d34180e33f4947
Jona <jonathanwears@outlook.com>
2023-03-03 12:47:17 +0100

    Update WordForm.jsx

--- src/components/FlashCard/WordForm.jsx
+++ src/components/FlashCard/WordForm.jsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import FormInput from './FormInput';
 import Button from '../Button';
 
-function WordForm() {
+const defaultValues = {
+  englishWord: '',
+  germanWord: '',
+};
+
+function WordForm({ values = defaultValues, handleSubmit }) {
+  const [words, setWords] = useState(values);
+
+  const handleOnChange = useCallback((event) => {
+    setWords((prevValue) => ({
+      ...prevValue,
+      [event.target.name]: event.target.value,
+    }));
+  });
+
+  function handleOnClick(event) {
+    event.preventDefault();
+    handleSubmit(words);
+  }
+
   return (
-    <div className="bg-blue-50 p-2 w-96 rounded-lg">
+    <div className="bg-blue-50 p-2 w-96 rounded-lg absolute">
       <form className="w-full flex flex-col justify-center items-center p-2">
-        <FormInput word="English" />
-        <FormInput word="German" />
-        <Button>Submit</Button>
+        <FormInput
+          word="english"
+          value={words.englishWord}
+          onChange={handleOnChange}
+        />
+        <FormInput
+          word="german"
+          value={words.germanWord}
+          onChange={handleOnChange}
+        />
+        <Button
+          onClick={handleOnClick}
+        >
+          Submit
+        </Button>
       </form>
     </div>
   );
