f8da952f14baf01f3896beaea4b5bb57cdfe0f57
Jona <jonathanwears@outlook.com>
2023-03-03 12:47:31 +0100

    Update FormInput.jsx

--- src/components/FlashCard/FormInput.jsx
+++ src/components/FlashCard/FormInput.jsx
@@ -1,19 +1,24 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 
-function FormInput({ word }) {
+function FormInput({ word, onChange, value }) {
   return (
-
     <label
       className="w-full"
       htmlFor={`${word}-word-input`}
     >
-      <p className="p-2 text-slate-800">{word} Word:</p>
+      <p
+        className="p-2 text-slate-800 capitalize"
+      >
+        {word} Word:
+      </p>
       <input
         className="text-slate-900 rounded-md border-2 w-full p-2 shadow-sm"
         type="text"
-        name={`${word}-word-input`}
-        placeholder={`${word} Word`}
+        name={`${word}Word`}
+        placeholder={`${word} word`}
+        onChange={onChange}
+        value={value}
       />
     </label>
 
