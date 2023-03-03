6be22772ababf35ce842163874cdc0562aec7ff0
Jona <jonathanwears@outlook.com>
2023-03-03 12:47:09 +0100

    Update Tile.jsx

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,23 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import FlashCardWord from './FlashCard/FlashCardWord';
+import GearIcon from '../icons/GearIcon';
+import EditFlashcard from './FlashCard/EditFlashcard';
+import EditFlashcardForm from './FlashCard/EditFlashcardForm';
 
-function Tile({ word }) {
-  const styles = 'flex items-center justify-center rounded-xl h-72 w-72 m-2 shadow-xl ';
+function Tile({ word, index }) {
+  const styles = 'flex flex-col justify-center items-center rounded-xl h-72 w-72 m-2 shadow-xl hover:opacity-90';
 
   const [languageToggle, setLanguageToggle] = useState(false);
   const [style, setStyle] = useState(styles);
+  const [edit, setEdit] = useState(false);
+
+  const bgStyle = languageToggle ? 'bg-blue-300 rounded-full' : 'bg-red-500';
+
+  useEffect(() => {
+    const newStyle = styles.concat(' ', bgStyle);
+    setStyle(newStyle);
+  }, [languageToggle]);
 
   function handleOnClick() {
     setLanguageToggle(!languageToggle);
   }
 
   const shownWord = languageToggle ? word.englishWord : word.germanWord;
-  const bgStyle = languageToggle ? 'bg-blue-300 rounded-full' : 'bg-red-400';
 
-  useEffect(() => {
-    const a = styles.concat(' ', bgStyle);
-    setStyle(a);
-  }, [languageToggle]);
+  const editForm = edit ? (
+    <EditFlashcardForm
+      word={word}
+      index={index}
+    />
+  ) : null;
 
   return (
     <div
@@ -26,7 +38,17 @@ function Tile({ word }) {
     >
       <FlashCardWord
         word={shownWord}
+        edit={GearIcon}
       />
+      <div className=" pt-28">
+        <EditFlashcard
+          edit={edit}
+          setEdit={setEdit}
+        />
+      </div>
+      <div>
+        {editForm}
+      </div>
     </div>
   );
 }
