99028af7243a0884237bfaea446f88deb16b46b5
Jona <jonathanwears@outlook.com>
2023-05-26 17:51:12 +0200

    moved parts out

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,15 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import FlashCardWord from './FlashCard/FlashCardWord';
-import GearIcon from '../icons/GearIcon';
 import EditFlashcard from './FlashCard/EditFlashcard';
-import EditFlashcardForm from './FlashCard/EditFlashcardForm';
 
 function Tile({ word, index }) {
   const styles = 'flex flex-col justify-center items-center rounded-xl h-72 w-72 m-2 shadow-xl hover:opacity-90';
 
   const [languageToggle, setLanguageToggle] = useState(false);
   const [style, setStyle] = useState(styles);
-  const [edit, setEdit] = useState(false);
 
   const bgStyle = languageToggle ? 'bg-blue-300 rounded-full' : 'bg-red-500';
 
@@ -24,13 +21,6 @@ function Tile({ word, index }) {
 
   const shownWord = languageToggle ? word.englishWord : word.germanWord;
 
-  const editForm = edit ? (
-    <EditFlashcardForm
-      word={word}
-      index={index}
-    />
-  ) : null;
-
   return (
     <div
       className={style}
@@ -38,16 +28,12 @@ function Tile({ word, index }) {
     >
       <FlashCardWord
         word={shownWord}
-        edit={GearIcon}
       />
       <div className=" pt-28">
         <EditFlashcard
-          edit={edit}
-          setEdit={setEdit}
+          word={word}
+          index={index}
         />
-      </div>
-      <div>
-        {editForm}
       </div>
     </div>
   );
