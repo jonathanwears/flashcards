00824e5a6d365257bb34f4077d1f3805072a31d7
Jona <jonathanwears@outlook.com>
2023-06-05 19:22:08 +0200

    Name change and slight changes to CSS

---
+++ src/components/FlashCard/Card.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import FlashCardWord from './FlashCardWord';
+import EditFlashcard from './EditFlashcard';
+
+function Card({ word, index }) {
+  const [languageToggle, setLanguageToggle] = useState(false);
+
+  const className = classNames('flex flex-col select-none justify-center items-center rounded-3xl h-96 w-72 m-2 shadow-xl hover:opacity-90', {
+    'bg-blue-300': languageToggle,
+    'bg-red-500': !languageToggle,
+  });
+
+  function handleOnClick() {
+    setLanguageToggle(!languageToggle);
+  }
+
+  const shownWord = languageToggle ? word.englishWord : word.germanWord;
+
+  return (
+    <div className="relative">
+      <div
+        className={className}
+        onClick={handleOnClick}
+      >
+        <FlashCardWord
+          word={shownWord}
+        />
+      </div>
+      <div className=" pt-28 absolute bottom-6 right-1/2">
+        <EditFlashcard
+          word={word}
+          index={index}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Card;
