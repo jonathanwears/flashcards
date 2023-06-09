9b5a1a5bdf1e7dca2b8de3a53cf42c84a6935449
Jona <jonathanwears@outlook.com>
2023-06-09 14:36:23 +0200

    commented out server calls

--- src/components/FlashCard/EditFlashcardForm.jsx
+++ src/components/FlashCard/EditFlashcardForm.jsx
@@ -1,28 +1,27 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import useDataStore from '../../utils/useDataStore';
 import { updateWord } from '../../utils/server/index';
 import WordForm from './WordForm';
 
-// Creates a component to add a new word to db and edit existing word
 function EditFlashcardForm({ word, postMethod, index }) {
   const setNewWord = useDataStore((state) => state.replaceWord);
-  const findWord = useDataStore((state) => state.words[index]);
-  const history = useHistory();
-  const [value, setNewValue] = useState();
 
-  const postOptions = {
-    method: postMethod,
-    body: JSON.stringify(value),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
+  // const findWord = useDataStore((state) => state.words[index]);
+  // const history = useHistory();
+  // const [value, setNewValue] = useState();
+
+  // const postOptions = {
+  //   method: postMethod,
+  //   body: JSON.stringify(value),
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  // };
 
   async function handleSubmitForm(words) {
     try {
-      console.log(words);
       setNewWord(index, words);
       // await updateWord(postOptions);
     } catch (err) {
