e44fbc400c7338ecf43f408455392ed34083bafc
Jona <jonathanwears@outlook.com>
2023-06-15 17:43:23 +0200

    added styling and structure

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -26,21 +26,29 @@ function Game() {
   };
 
   return (
-    <div className="grid grid-cols-5">
-      <Card
-        word={mockData[count]}
-      />
-      <Button
-        onClick={handleOnClick}
-      >
-        Next Card
-      </Button>
-      <Score
-        score={score}
-      />
-      <GameButtons
-        onClick={handleAnswer}
-      />
+    <div className="grid grid-cols-9 auto-rows-fr">
+      <div className="col-span-3 row-span-3 row-start-2 col-start-2">
+        <Card
+          word={mockData[count]}
+        />
+      </div>
+      <div className="flex col-start-5 row-start-3 col-span-2 justify-center items-center">
+        <Button
+          onClick={handleOnClick}
+        >
+          Next Card
+        </Button>
+      </div>
+      <div className="row-span-3 col-span-2 row-start-2">
+        <Score
+          score={score}
+        />
+      </div>
+      <div className="row-start-6 col-start-2 col-span-3">
+        <GameButtons
+          onClick={handleAnswer}
+        />
+      </div>
     </div>
   );
 }
