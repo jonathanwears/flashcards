c62c3ddbcefdcccbf851255e0564c507d81a996e
Jona <jonathanwears@outlook.com>
2023-06-16 12:02:16 +0200

    centered button

--- src/pages/Database.jsx
+++ src/pages/Database.jsx
@@ -21,7 +21,9 @@ function Database() {
 
   return (
     <div className="flex flex-col">
-      <AddNewWord />
+      <div className="flex justify-center">
+        <AddNewWord />
+      </div>
       <div className="flex justify-center">
         {renderWords}
       </div>
